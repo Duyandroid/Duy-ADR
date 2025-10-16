3a8df70db58b76a25f1e0ca141854e1856710264
Duyandroid <buitruongduya03@gmail.com>
2025-10-15 18:47:41 -0700

    testscript

---
+++ Test.lua
@@ -0,0 +1,7 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+
+local Window = redzlib:MakeWindow({
+  Title = "Duy Android Hub",
+  SubTitle = "by Duy Trường",
+  SaveFolder = "Redz | redz lib v5.lua"
+})
